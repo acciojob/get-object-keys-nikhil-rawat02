a6bad342d45cfcfaaae50ccb5fc0d88381707b2e
Nikhil Rawat <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-21 11:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If requile
+let student = { name:""}
+
+Object.prototype.getKeys = function{
+	let this.arr = []
+	for(let key in this.Object){
+		this.arr.push(key);
+	}
+	return this.arr;
+}
